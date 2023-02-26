50b5048cc3e67efb779dc4abe3a6a9d9d7294af1
Dany Paredes <dany.paredes@external.wolterskluwer.com>
2023-02-26 12:50:56 +0100

    add type orm

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Product {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   price: string;
+
+  @Column('json', { nullable: true })
   categories: string[];
 }
